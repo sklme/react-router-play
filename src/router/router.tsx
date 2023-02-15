6774d3756044ef861704c1e1ee9bd9dd35edf27a
wrynnsun <wrynnsun@tencent.com>
2023-02-15 20:58:44 +0800

    feat: add contact router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from '~/routes/root';
+import ErrorPage from '~/error-page';
+import Contact from '~/routes/contact';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+  },
+  {
+    path: 'contacts/:contactId',
+    element: <Contact />,
   },
 ]);
 
