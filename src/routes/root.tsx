56e9b58a06507974c563a0c54575f1df30a49b69
wrynnsun <wrynnsun@tencent.com>
2023-02-16 15:15:23 +0800

    feat: NavLink

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,10 @@
-import { Outlet, Link, useLoaderData, Form, redirect } from 'react-router-dom';
+import {
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  redirect,
+} from 'react-router-dom';
 import { getContacts, createContact } from '~/contact';
 
 export async function loader() {
@@ -42,7 +48,12 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'pending' : isPending ? 'pending' : ''
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -51,7 +62,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{' '}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
